879e90203471e0f1593be977feaac4f178d23d16
Telman Yusifov <telmanyusifov78@gmail.com>
2021-05-11 12:53:17 +0400

    Some unnecessary deleted.

--- BakcellPhoneWebApp/App_Start/BundleConfig.cs
+++ BakcellPhoneWebApp/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace BakcellPhoneWebApp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
